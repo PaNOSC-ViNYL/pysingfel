5b9e895b5cd3c77b71d9a259af49418b6d039faf
Carsten Fortmann-Grote <carsten.grote@xfel.eu>
2017-10-12 16:37:12 +0200

    >20 % performance with guvectorize.

--- pysingfel/diffraction.py
+++ pysingfel/diffraction.py
@@ -1,5 +1,6 @@
 import numba as nb
-from numba import jit
+import math
+from numba import jit, guvectorize, float64, c16
 from scipy.interpolate import CubicSpline
 
 from pysingfel.detector import *
@@ -16,9 +17,9 @@ def calculate_Thomson(ang):
 def calculate_atomicFactor(particle, detector):
     """
     particle.ffTable contains atomic form factors against particle.qSample.
-    Here the atomic form factors related to the wavevector of each pixel of 
+    Here the atomic form factors related to the wavevector of each pixel of
     the detector is calculated through interpolation to the ffTable data.
-    
+
     Return:
         f_hkl: atomic form factors related to each pixel of detector for each atom type.
     """
@@ -29,35 +30,70 @@ def calculate_atomicFactor(particle, detector):
         f_hkl[:, :, atm] = cs(q_mod_Bragg)  # interpolate
     return f_hkl
 
+#def Phase(atomPos,  q_xyz, angles):
+    #for ix in range(q_xyz.shape[0]):
+        #for iy in range(q_xyz.shape[1]):
+            #for icart in range(3):
+                #re[ix, iy] += atomPos[icart] * q_xyz[ix, iy, icart]
 
-@jit
-def Phase(atomPos,  q_xyz):
-    phase = 2 * np.pi * (atomPos[0] * q_xyz[:, :, 0] + atomPos[1] * q_xyz[:, :, 1] + atomPos[2] * q_xyz[:, :, 2])
-    return np.exp(1j * phase)
 
+# ~20% improved wrt original implementation
+#@jit
+#def cal(f_hkl, atomPos, q_xyz, xyzInd):
+    #F_re = np.zeros_like(q_xyz[:, :, 0], dtype=nb.double)
+    #F_im = np.zeros_like(q_xyz[:, :, 0], dtype=nb.double)
+    #for atm in range(atomPos.shape[0]):
+        #for ix in range(F_re.shape[0]):
+            #for iy in range(F_re.shape[1]):
+                #angle = 2.*np.pi * ( atomPos[atm,0] * q_xyz[ix, iy, 0] +  atomPos[atm,1] * q_xyz[ix, iy, 1] + atomPos[atm,2] * q_xyz[ix, iy, 2])
+                #f = f_hkl[ix, iy, xyzInd[atm]]
+                #F_re[ix, iy] +=  np.cos(angle) * f
+                #F_im[ix, iy] +=  np.sin(angle) * f
 
-@jit
-def cal(f_hkl, atomPos, q_xyz, xyzInd):
-    F = np.zeros_like(q_xyz[:, :, 0], dtype=nb.c16)
+    #for ix in range(F_re.shape[0]):
+        #for iy in range(F_re.shape[1]):
+            #F_re[ix, iy] = F_re[ix, iy]**2 + F_im[ix, iy]**2
+
+    #return F_re
+
+# >20% improved wrt original implementation
+@guvectorize(["(float64[:,:,:], float64[:,:,:], float64[:,:], int32[:], float64[:,:])"],
+        "(n,m,l),(n,m,k),(j,k),(j) -> (n,m)", target="parallel")
+def cal(f_hkl, q_xyz, atomPos, xyzInd, F):
+
+    F_im = np.zeros_like(F)
+
     for atm in range(atomPos.shape[0]):
-        F += Phase(atomPos[atm, :], q_xyz) * f_hkl[:, :, xyzInd[atm]]
-    return np.abs(F)**2
+        for ix in range(F.shape[0]):
+            for iy in range(F.shape[1]):
+                angle = 2.*np.pi * ( atomPos[atm,0] * q_xyz[ix, iy, 0] +  atomPos[atm,1] * q_xyz[ix, iy, 1] + atomPos[atm,2] * q_xyz[ix, iy, 2])
+                f = f_hkl[ix, iy, xyzInd[atm]]
+                F[ix, iy] +=  np.cos(angle) * f
+                F_im[ix, iy] +=  np.sin(angle) * f
+
+    for ix in range(F.shape[0]):
+        for iy in range(F.shape[1]):
+            F[ix, iy] = F[ix, iy]**2+ F_im[ix, iy]**2
 
 
 def calculate_molecularFormFactorSq(particle, detector):
     """
-    Calculate molecular form factor for each pixel of detector to get diffraction pattern. 
+    Calculate molecular form factor for each pixel of detector to get diffraction pattern.
     Sum over all atoms with the right phase factor.
     See https://www.nature.com/article-assets/npg/srep/2016/160425/srep24791/extref/srep24791-s1.pdf
     for more details about the derivation (equ. 13 is used for calculation here).
     """
     f_hkl = calculate_atomicFactor(particle, detector)
     s = particle.SplitIdx
-    xyzInd = np.zeros(s[-1], dtype=int)
+    xyzInd = np.zeros(s[-1], dtype=np.int32)
+    F = np.zeros((detector.q_xyz.shape[0], detector.q_xyz.shape[1]), dtype=np.float64)
+
     for i in range(len(s)-1):
         xyzInd[s[i]:s[i+1]] = i
-    return cal(f_hkl, particle.atomPos, detector.q_xyz, xyzInd)
 
+    cal(f_hkl, detector.q_xyz, particle.atomPos, xyzInd, F)
+
+    return F
 
 def calculate_compton(particle, detector):
     """
