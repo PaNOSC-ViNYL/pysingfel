a2a34fdb1056fe558c272f92cd430dd01b0d2c91
Carsten Fortmann-Grote <carsten.grote@xfel.eu>
2017-11-21 13:10:33 +0100

    Fixing CI build script.

--- CI/build_and_test.sh
+++ CI/build_and_test.sh
@@ -3,13 +3,14 @@ set -e # Exit with nonzero exit code if anything fails
 
 printenv
 
-cd $VIRTUAL_ENV/tests
+export TEST_DIR=$TRAVIS_BUILD_DIR/tests
+cd $TEST_DIR
 
 # Build
-export PYTHONPATH=..
+export PYTHONPATH=$TRAVIS_BUILD_DIR:$PYTHONPATH
 echo $PYTHONPATH
 
-export PATH=../bin:$PATH
+export PATH=$TRAVIS_BUILD_DIR/bin:$PATH
 echo $PATH
 
 # unit tests
